6e09aaac2b0fe41a71e9d2e5e57a4bb71ce854ac
fundorin174 <fundorin174@gmail.com>
2025-10-31 15:23:53 +0300

    Start готовое приложение без роутинга

---
+++ lessons/lesson14/projects/configuringRoutes/src/App.tsx
@@ -0,0 +1,22 @@
+﻿import { Provider } from 'react-redux';
+import { store } from './store';
+import Header from './components/Header';
+import ProductCatalog from './components/ProductCatalog';
+import Cart from './components/Cart';
+import styles from './styles/App.module.css';
+
+const App = () => {
+  return (
+    <Provider store={store}>
+      <div className={styles.app}>
+        <Header />
+        <main className={styles.main}>
+          <ProductCatalog />
+          <Cart />
+        </main>
+      </div>
+    </Provider>
+  );
+};
+
+export default App;
